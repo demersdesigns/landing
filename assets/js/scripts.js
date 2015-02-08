a5aa9a10745760f6a9c9eea7bd425fa9967831cc
Paul Demers <paul@demersdesigns.com>
2015-02-08 16:31:13 -0500

    Simple redesign

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,3 @@
-//Site scripts
-$(function(){
-	var today = new Date();
-
-	if (today.getHours() >= 17) {
-		$('.morn').css('opacity', 0.5);
-	} else {
-		$('.eve').css('opacity', 0.5);
-	}
-});
+/*global $:false */
+'use strict';
+(function(){})();
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,4 @@
+'use strict';
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
@@ -20,13 +21,7 @@
         }
     }
 }());
-//Site scripts
-$(function(){
-	var today = new Date();
 
-	if (today.getHours() >= 17) {
-		$('.morn').css('opacity', 0.5);
-	} else {
-		$('.eve').css('opacity', 0.5);
-	}
-});
+/*global $:false */
+'use strict';
+(function(){})();
